8fbdb00067329a7363558ebf3d58dbc3c8c83db6
astronik00 <astronik00@gmail.com>
2022-11-15 16:16:42 +0300

    Added basic auth

---
+++ src/main/java/com/example/simplevkapp/configurations/BasicAuthWebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.simplevkapp.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class BasicAuthWebSecurityConfig
+{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .authorities("ROLE_ADMIN");
+    }
+}
